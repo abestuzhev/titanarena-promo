df0052de3868e849a134980306be7345b9e8bdfb
abestuzhev <aleksey.bestuzhev@gmail.com>
2017-09-25 13:07:35 +0400

    changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $(window).load(function(){
     $(".c-input").val("");
 
-    $(".input-effect input").focusout(function(){
+    $(document).on('focusout', ".input-effect input", function(){
       if($(this).val() != ""){
         $(this).addClass("has-content");
       }else{
@@ -38,4 +38,88 @@ $(document).ready(function(){
       $(this).siblings('.c-input').attr('type', 'text');
   });
 
+
+    /*плавный скролл*/
+    $(".с-btn--reg, .c-btn-down").click(function(e){
+        e.preventDefault();
+        var hash = $(this).attr("href");
+        var target = $(hash);
+
+        $("html, body").stop().animate({
+            scrollTop: target.offset().top
+        }, 300, function(){
+            window.location.hash = hash;
+        });
+
+    });
+
+    /*маска*/
+    $('.c-input--date').mask('00.00.0000');
+    $('.c-input--tel').mask('+7(000)000-00-00');
+    $('.c-input--sum').mask('000000');
+
+
+    /*добавление новой покупки на главной*/
+    $(document).on('click','.c-btn-add', function(e){
+        e.preventDefault();
+        var countBox = $(".purchase-layout").length+1;
+        console.log('Количество элементов' + countBox);
+        var $accentBoxTemplate = $(
+            '<div class="c-form-list purchase">' +
+            '<div class="c-form-item">' +
+            '<input type="search" id="brand-filter-' + countBox +'" placeholder="Найти отдел.." name="brand-filter-' + countBox +'" data-list="brands-list" autocomplete="off" required />' +
+            '<label for="brand-filter-' + countBox +'" data-icon="&#128269;"></label>' +
+            '<datalist id="brands-list">' +
+            '<select>' +
+            '<option value="Подио" />' +
+            '<option value="Кари" />' +
+            '<option value="Билайн" />' +
+            '<option value="Westfalika" />' +
+            '<option value="Мартиника" />' +
+            '<option value="Клинок" />' +
+            '<option value="Soho" />' +
+            '<option value="Московский Ювелирный завод" />' +
+            '<option value="Amati" />' +
+            '<option value="Эконика" />' +
+            '<option value="МТС" />' +
+            '<option value="Кабинотье" />' +
+            '<option value="ИЛЬ ДЕ БОТЕ" />' +
+            '<option value="Coffe Man" />' +
+            '<option value="Органик Маркет" />' +
+            '<option value="Бюстье" />' +
+            '<option value="Leo Ventoni" />' +
+            '<option value="Связной" />' +
+            '<option value="ГрандПри" />' +
+            '<option value="Мультиварка" />' +
+            '<option value="Zinger" />' +
+            '<option value="Пандора" />' +
+            '<option value="Аптека МК" />' +
+            '<option value="Адамас" />' +
+            '<option value="Mascotte" />' +
+            '<option value="Четыре лапы" />' +
+            '<option value="Harold" />' +
+            '<option value="РивГош" />' +
+            '<option value="G Store" />' +
+            '<option value="Мегафон" />' +
+            '<option value="H&M" />' +
+            '<option value="Vitacci" />' +
+            '</select>' +
+            '</datalist>' +
+            '</div>' +
+            '<div class="c-form-item input-effect">' +
+            '<input type="text" class="c-input effect-17" name="purchase-amount-' + countBox +'">' +
+            '<label class="c-label">Сумма покупки</label>' +
+            '</div>' +
+            '<div class="c-form-item input-effect">' +
+            '<input type="text" class="c-input effect-17" name="date-amount-' + countBox +'">' +
+            '<label class="c-label">Дата</label>' +
+            '</div>' +
+            '</div>'
+        );
+        $('.purchase-layout').append($accentBoxTemplate).find('.purchase:last-child');
+
+        $('.c-input--date').mask('00.00.0000');
+        $('.c-input--tel').mask('+7(000)000-00-00');
+        $('.c-input--sum').mask('000000');
+    });
 });
